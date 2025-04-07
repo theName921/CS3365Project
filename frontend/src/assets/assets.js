f84f38ed3b58bed55f1ef3e04de11d25e376e4ee
theName921 <drakesethwyatt@gmail.com>
2025-04-07 02:11:04 -0500

    initial commit

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,32 @@
+ import placeholder from "./placeholder_image.png"
+ import logo from "./placeholder_logo.png"
+ import search_icon from "./search_icon.png"
+ import user_icon from "./user_icon.png"
+ import cart_icon from "./cart_icon.png"
+ import menu_icon from "./menu_icon.png"
+ import back_icon from "./back_icon.png"
+ 
+ export const assets = {
+    placeholder,
+    logo,
+    search_icon,
+    user_icon,
+    cart_icon,
+    menu_icon,
+    back_icon
+ }
+
+ export const products = [
+    {
+        _id: "aaaaa",
+        name: "This is a test product",
+        description: "This is a test description",
+        price: 100,
+        image: [placeholder],
+        category: "ThisOne",
+        subCategory: "SubThisOne",
+        sizes: ["S", "M", "L"],
+        date: 412396783241,
+        bestseller: true
+    }
+ ]

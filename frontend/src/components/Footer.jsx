34a383dc0d23aea916ed1a671d7296e4efbd092c
theName921 <drakesethwyatt@gmail.com>
2025-04-11 12:59:24 -0500

    Finsihed Home page

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,11 +7,35 @@ const Footer = () => {
     <div>
       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
         <div>
-            <p className='w-full md:w-2/3 text-gray-600'>
-            E-Commerce Project :: Drake Wyatt
-            </p>
-            </div>
+          <p className='w-full md:w-2/3 text-gray-600'>
+          E-Commerce Project :: Drake Wyatt
+          </p>
+        </div>
+
+        <div>
+          <p className='textxl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-1 text-gray-600'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-xl foint-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-1 text-gray-600'>
+            <li>+1-212-456-7890</li>
+            <li>contact@placeholder.com</li>
+          </ul>
+        </div>
       </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2025@ placeholder.com - All Rights Reserved.</p>
+      </div>
+
     </div>
   )
 }
